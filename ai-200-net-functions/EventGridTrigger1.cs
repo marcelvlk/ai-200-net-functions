205090a895269caf776a05184b2e0f3198459ae7
marcelvlk <marcel.vlk@wolverinedevops.com>
2026-07-16 12:38:36 +0200

    Updated EventGridTrigger1 function.

--- ai-200-net-functions/EventGridTrigger1.cs
+++ ai-200-net-functions/EventGridTrigger1.cs
@@ -1,10 +1,11 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
-using System;
 using Azure.Messaging;
+using Azure.Messaging.EventGrid;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace ai_200_net_functions;
 
@@ -18,9 +19,9 @@ public class EventGridTrigger1
     }
 
     [Function(nameof(EventGridTrigger1))]
-    public void Run([EventGridTrigger] CloudEvent cloudEvent)
+    public void Run([EventGridTrigger] EventGridEvent eventGridEvent)
     {
         _logger.LogInformation("C# EventGridTrigger1 function triggered.");
-        _logger.LogInformation("Event type: {type}, Event subject: {subject}", cloudEvent.Type, cloudEvent.Subject);
+        _logger.LogInformation("Event type: {type}, Event subject: {subject}", eventGridEvent.EventType, eventGridEvent.Subject);
     }
 }
